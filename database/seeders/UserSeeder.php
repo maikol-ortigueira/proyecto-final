0f75c5cacd54bb76a21c9079c85498b14c8f322d
maikol-ortigueira <maikol.ortigueira@gmail.com>
2022-03-19 02:15:58 +0100

    Añadido usuario superadmin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,8 +26,17 @@ class UserSeeder extends Seeder
             'password' => '$2y$10$MC8bbxmYp6NX8P4W6mKo4ertnh.OB0hBPAhghrQNuO90yToEwKnRi',
             'remember_token' => \Illuminate\Support\Str::random(10),
         ])->roles()->sync([1]);
+
+        $otroUsuario = User::create([
+            'name' => 'Marcos',
+            'email' => 'marcos@marcos.prueba',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => \Illuminate\Support\Str::random(10),
+        ])->roles()->sync([1]);
         
         $miPerfil = Perfil::factory(1)->state(['user_id' => '1'])->create();
+        $otroPerfil = Perfil::factory(1)->state(['user_id' => '2'])->create();
 
         $users = User::factory(10)->hasPerfil()->create();
         foreach ($users as $user) {
